34415fae5b3c87ba5ce7545213e5798982936ecb
kimtx200 <kimtx200@gmail.com>
2016-10-26 13:01:58 +0900

    #simple watcha init-v0.1

---
+++ Taewon-test/SimpleWatcha/app/src/main/java/study/com/ted/simplewatcha/RankingActivity.java
@@ -0,0 +1,13 @@
+package study.com.ted.simplewatcha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
